3f17bc4b65015fe8300a7f80be97c8fdb898d2ee
corinceSub <hanwang3@andrew.cmu.edu>
2025-04-27 23:24:00 -0400

    Updated on jumping and moving mechanics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 7f;
+    public float moveSpeed = 8f;
+    public float jumpForce = 12f;
+    public float acceleration = 20f;
+    public float deceleration = 25f;
+    public float velocityPower = 0.9f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private bool isGrounded;
+    private float moveInput;
+    private bool jumpPressed;
 
     void Start()
     {
@@ -17,31 +24,48 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float moveX = 0f;
+        moveInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveX = -1f;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            moveX = 1f;
-        }
-
-        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
-
-        if (moveX > 0)
+        if (moveInput > 0)
         {
             spriteRenderer.flipX = false;
         }
-        else if (moveX < 0)
+        else if (moveInput < 0)
         {
             spriteRenderer.flipX = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            jumpPressed = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float targetSpeed = moveInput * moveSpeed;
+        float speedDifference = targetSpeed - rb.linearVelocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDifference) * accelRate, velocityPower) * Mathf.Sign(speedDifference);
+
+        rb.AddForce(movement * Vector2.right);
+
+        rb.linearVelocity = new Vector2(Mathf.Clamp(rb.linearVelocity.x, -moveSpeed, moveSpeed), rb.linearVelocity.y);
+
+        if (jumpPressed)
+        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+            jumpPressed = false;
+        }
+
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
     }
 
